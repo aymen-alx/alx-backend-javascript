254a9144f8dfffa83fc9063955147d7c858f41b2
Aymen <aymenabduseid10@gmail.com>
2024-09-20 21:10:30 +0300

    init

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,15 +1,16 @@
-const sinon = require('sinon');
-const Utils = require('./utils');
 const { expect } = require('chai');
+const sinon = require('sinon');
+const { describe, it } = require('mocha');
+const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', () => {
-  it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
-    const spy = sinon.spy(Utils);
-
+  const spy = sinon.spy(Utils, 'calculateNumber');
+  it('sinon first test', () => {
     sendPaymentRequestToApi(100, 20);
-    expect(spy.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
-    expect(spy.calculateNumber.callCount).to.be.equal(1);
-    spy.calculateNumber.restore();
+    expect(spy.firstCall.args[0]).to.equal('SUM');
+    expect(spy.firstCall.args[1]).to.equal(100);
+    expect(spy.firstCall.args[2]).to.equal(20);
+    spy.restore();
   });
 });
